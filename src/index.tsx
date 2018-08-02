ad2bb9ab8420150ac4a595e1a9683f886c76773f
cag2050 <cag2050@163.com>
2018-08-02 09:57:08 +0800

    m

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from './button';
+import { Container } from './container';
+import { Icon } from './icon';
+import { Select } from './select';
+import { Switch } from './switch';
+
+import 'antd/dist/antd.css';
+
+export default [Container, Button, Select, Icon, Switch] as any[];
